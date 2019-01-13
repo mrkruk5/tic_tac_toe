7b03df4f9806f8645284dac22579e86e134356c1
mrkruk5 <mrkruk5@gmail.com>
2019-01-12 21:10:40 -0600

    Changed control flow to comply with generally standard game design and used global variables to save states.

--- Tic-Tac-Toe/tic-tac-toe.cpp
+++ Tic-Tac-Toe/tic-tac-toe.cpp
@@ -1,18 +1,23 @@
 #include <iostream>
+bool quit = false;
+char b[9] = { ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ' };
+bool p1 = true; // true = p1, false = p2
+int p1_input;
+int p2_input;
 
-void printBoard(char b[9]) {
+void printBoard() {
 	for (int i = 0; i < 9; i++) {
 		if ((i + 1) != 9 && (i + 1) % 3 == 0) {
-			std::cout << b[i] << std::endl;
+			std::cout << ::b[i] << std::endl;
 			for (int j = 0; j < 3; j++) {
 				std::cout << "- ";
 			}
 			std::cout << std::endl;
 		} else {
 			if (i != 8) {
-				std::cout << b[i] << "|";
+				std::cout << ::b[i] << "|";
 			} else {
-				std::cout << b[i];
+				std::cout << ::b[i];
 			}
 
 		}
@@ -20,43 +25,43 @@ void printBoard(char b[9]) {
 	std::cout << std::endl;
 }
 
-bool checkForWin(char b[9]) {
-	if (b[0] != ' ' && b[1] != ' ' && b[2] != ' ' && b[0] == b[1] && b[1] == b[2]) {
+bool win() {
+	if (::b[0] != ' ' && ::b[1] != ' ' && ::b[2] != ' ' && ::b[0] == ::b[1] && ::b[1] == ::b[2]) {
 		return true;
-	} else if (b[3] != ' ' && b[4] != ' ' && b[5] != ' ' && b[3] == b[4] && b[4] == b[5]) {
+	} else if (::b[3] != ' ' && ::b[4] != ' ' && ::b[5] != ' ' && ::b[3] == ::b[4] && ::b[4] == ::b[5]) {
 		return true;
-	} else if (b[6] != ' ' && b[7] != ' ' && b[8] != ' ' && b[6] == b[7] && b[7] == b[8]) {
+	} else if (::b[6] != ' ' && ::b[7] != ' ' && ::b[8] != ' ' && ::b[6] == ::b[7] && ::b[7] == ::b[8]) {
 		return true;
-	} else if (b[0] != ' ' && b[3] != ' ' && b[6] != ' ' && b[0] == b[3] && b[3] == b[6]) {
+	} else if (::b[0] != ' ' && ::b[3] != ' ' && ::b[6] != ' ' && ::b[0] == ::b[3] && ::b[3] == ::b[6]) {
 		return true;
-	} else if (b[1] != ' ' && b[4] != ' ' && b[7] != ' ' && b[1] == b[4] && b[4] == b[7]) {
+	} else if (::b[1] != ' ' && ::b[4] != ' ' && ::b[7] != ' ' && ::b[1] == ::b[4] && ::b[4] == ::b[7]) {
 		return true;
-	} else if (b[2] != ' ' && b[5] != ' ' && b[8] != ' ' && b[2] == b[5] && b[5] == b[8]) {
+	} else if (::b[2] != ' ' && ::b[5] != ' ' && ::b[8] != ' ' && ::b[2] == ::b[5] && ::b[5] == ::b[8]) {
 		return true;
-	} else if (b[0] != ' ' && b[4] != ' ' && b[8] != ' ' && b[0] == b[4] && b[4] == b[8]) {
+	} else if (::b[0] != ' ' && ::b[4] != ' ' && ::b[8] != ' ' && ::b[0] == ::b[4] && ::b[4] == ::b[8]) {
 		return true;
-	} else if (b[2] != ' ' && b[4] != ' ' && b[6] != ' ' && b[2] == b[4] && b[4] == b[6]) {
+	} else if (::b[2] != ' ' && ::b[4] != ' ' && ::b[6] != ' ' && ::b[2] == ::b[4] && ::b[4] == ::b[6]) {
 		return true;
 	} else {
 		return false;
 	}
 }
 
-bool checkFull(char b[9]) {
+bool full() {
 	for (int i = 0; i < 9; i++) {
-		if (b[i] == ' ') {
+		if (::b[i] == ' ') {
 			return false;
 		}
 	}
 	return true;
 }
 
-bool validMove(char b[9], int i) {
+bool validMove(int i) {
 	if (0 <= i && i < 9) {
-		if (b[i] == ' ') {
+		if (::b[i] == ' ') {
 			return true;
 		} else {
-			std::cout << "There's already an " << b[i] << " there!" << std::endl;
+			std::cout << "There's already an " << ::b[i] << " there!" << std::endl;
 			return false;
 		}
 	} else {
@@ -66,45 +71,49 @@ bool validMove(char b[9], int i) {
 
 }
 
-void userInput(char board[9]) {
-	while (1) {
-		int p1_input;
-		int p2_input;
-		while (!checkForWin(board) && !checkFull(board)) {
-			std::cout << "Player 1: Where do you want to place your x on the board?";
-			std::cin >> p1_input;
-			if (validMove(board, p1_input)) {
-				board[p1_input] = 'x';
-				printBoard(board);
-				if (checkForWin(board)) {
-					std::cout << "Congratulations, Player 1 wins!" << std::endl;
-				}
-				break;
+void userInput() {
+	if (::p1) {
+		std::cout << "Player 1: Where do you want to place your x on the board?";
+		std::cin >> ::p1_input;
+	} else {
+		std::cout << "Player 2: Where do you want to place your o on the board?";
+		std::cin >> p2_input;
+	}
+}
+
+void simulate() {
+	if (p1) {
+		if (validMove(::p1_input)) {
+			::b[::p1_input] = 'x';
+			::p1 = false;
+			if (win()) {
+				std::cout << "Congratulations, Player 1 wins!" << std::endl;
+				printBoard();
+				::quit = true;
 			}
 		}
-		while (!checkForWin(board) && !checkFull(board)) {
-			std::cout << "Player 2: Where do you want to place your o on the board?";
-			std::cin >> p2_input;
-			if (validMove(board, p2_input)) {
-				board[p2_input] = 'o';
-				printBoard(board);
-				if (checkForWin(board)) {
-					std::cout << "Congratulations, Player 2 wins!" << std::endl;
-				}
-				break;
+	} else {
+		if (validMove(::p2_input)) {
+			::b[::p2_input] = 'o';
+			::p1 = true;
+			if (win()) {
+				std::cout << "Congratulations, Player 2 wins!" << std::endl;
+				printBoard();
+				::quit = true;
 			}
 		}
-		if (checkForWin(board)) {
-			break;
-		} else if (checkFull(board)) {
-			std::cout << "Game Over, the board is full and nobody won." << std::endl;
-			break;
-		}
+	}
+	if (!win() && full()) {
+		std::cout << "Game Over, the board is full and nobody won." << std::endl;
+		printBoard();
+		::quit = true;
 	}
 }
 
 int main() {
-	char board[9] = { ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ' };
-	printBoard(board);
-	userInput(board);
+	while (!quit) {
+		printBoard();
+		userInput();
+		simulate();
+	}
 }
